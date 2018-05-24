0a1a7ffddf2e508599d468282f0993f52d84fbb5
albert18 <caps18@hotmail.com>
2018-05-24 17:42:57 -0400

    create components

---
+++ angular6Application/src/app/shopping-list/shopping-edir/shopping-edir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edir',
+  templateUrl: './shopping-edir.component.html',
+  styleUrls: ['./shopping-edir.component.css']
+})
+export class ShoppingEdirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

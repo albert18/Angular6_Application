73c1ff6ab533619313d151d7a1e10196b7d2cb79
albert18 <caps18@hotmail.com>
2018-05-25 08:14:18 -0400

    Recipe details

--- angular6Application/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular6Application/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Fish Recipe', 'Salmon with lemon', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+    new Recipe('Taco Recipe', 'Taco Fiesta', 'https://www.landolakes.com/lolretail/media/LOLR-Media/Recipe-Collections/easy-dinner-recipes-for-kids.jpg?ext=.jpg')
+  ];
 
   constructor() { }
 

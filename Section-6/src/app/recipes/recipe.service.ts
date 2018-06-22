43bc95c00c31604e68d6971c82ee711b56fcf595
albert18 <caps18@hotmail.com>
2018-06-21 21:20:11 -0400

    Cross Component Communicatio L-108

---
+++ Section-6/src/app/recipes/recipe.service.ts
@@ -0,0 +1,17 @@
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+}
